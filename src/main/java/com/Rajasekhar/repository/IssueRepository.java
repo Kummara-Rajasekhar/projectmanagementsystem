cdf670af565928324c8e159df563c25ac6428e69
K Rajasekhar <kummararajasekhar17092004@gmail.com>
2025-09-22 10:22:55 +0530

    Day 12

--- src/main/java/com/Rajasekhar/repository/IssueRepository.java
+++ src/main/java/com/Rajasekhar/repository/IssueRepository.java
@@ -3,6 +3,10 @@ package com.Rajasekhar.repository;
 import com.Rajasekhar.model.Issue;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IssueRepository extends JpaRepository<Issue, Long> {
 
+
+    public List<Issue> findByProjectID(Long id);
 }
